8cbde7f46c242fa035d9c6a81059e2b8592c856e
zahra <zaramoon99123@gmail.com>
2024-04-26 21:14:17 +0330

    knockout

---
+++ app.js
@@ -0,0 +1,43 @@
+//introduction
+/*function AppViewModel() {
+    this.firstName = "Bert";
+    this.lastName = "Bertington";
+}
+ko.applyBindings(new AppViewModel());*/
+
+
+// Making the data editable----obser----
+/*function AppViewModel() {
+    this.firstName =ko.observable( "Bert");
+    this.age =ko.observable("Bertington");
+}
+ko.applyBindings(new AppViewModel());*/
+
+
+//Defining computed values
+/*function AppViewModel() {
+    this.firstName =ko.observable( "Bert");
+    this.age =ko.observable("Bertington");
+    this.fullName =ko.computed(function(){
+        return this.firstName()+" "+this.age()
+    },this);
+}
+ko.applyBindings(new AppViewModel());*/
+
+
+//capitalizeLastName 
+function AppViewModel() {
+    this.firstName =ko.observable( "Bert");
+    this.age =ko.observable("Bertington");
+    this.fullName =ko.computed(function(){
+        return this.firstName()+" "+this.age()
+    },this);
+
+    this.capitalizeLastName=function(){
+        var valu=this.firstName()
+        this.firstName(valu.toUpperCase(0))
+    }
+}
+
+
+ko.applyBindings(new AppViewModel());
